0172dda55bb7badcf8ce6c4a097c41fb137cd67e
Megira <regenerat.55@mail.ru>
2024-06-21 20:13:51 +0300

    Отредактировал форму

--- views/requests/_form.php
+++ views/requests/_form.php
@@ -12,15 +12,12 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
-
     <?= $form->field($model, 'master_id')->textInput() ?>
 
     <?= $form->field($model, 'date')->textInput() ?>
 
     <?= $form->field($model, 'time')->textInput() ?>
 
-    <?= $form->field($model, 'status_id')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
