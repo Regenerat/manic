0f9b50624ab18cc63c8925e6d410d38239e138d9
Megira <regenerat.55@mail.ru>
2024-06-21 20:57:46 +0300

    Добавил админку

--- views/requests/index.php
+++ views/requests/index.php
@@ -1,6 +1,8 @@
 <?php
 
 use app\models\Requests;
+use app\models\Roles;
+use app\models\Status;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
@@ -20,12 +22,32 @@ $this->title = 'Requests';
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
-
             'user',
             'master',
             'date',
             'time',
             'status',
+            [
+                'attribute' => 'Сменить статус',
+                'visible' => (Yii::$app->user->identity->role_id == Roles::ADMIN_ROLE ? true : false),
+                'format' => 'raw',
+                'value' => function($model){
+
+                    if($model->status_id == Status::NEW_STATUS){
+                        $html = Html::beginForm(Url::to(['update', 'id' => $model->id]));
+                        $html .= Html::activeDropDownList($model, 'status_id',[
+                            '2' => 'Принять',
+                            '3' => 'Отклонить',
+                        ]);
+
+                        $html .= Html::submitButton('Save', ['class' => 'btn btn-success']);
+                        $html .= Html::endForm();
+                        return $html;
+                    }
+                    return "";
+                }
+            ],
+
         ],
     ]); ?>
 
