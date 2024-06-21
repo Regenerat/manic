cd2389a2119479b4e2bac890da77563375508df8
Megira <regenerat.55@mail.ru>
2024-06-21 19:41:38 +0300

    Добавил авторизацию

--- models/Users.php
+++ models/Users.php
@@ -17,7 +17,7 @@ use Yii;
  * @property Requests[] $requests
  * @property Roles $role
  */
-class Users extends \yii\db\ActiveRecord
+class Users extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -73,5 +73,67 @@ class Users extends \yii\db\ActiveRecord
     public function getRole()
     {
         return $this->hasOne(Roles::class, ['id' => 'role_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return static::find()->where(['id' => $id])->one();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return null;
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByLogin($login)
+    {
+        return static::find()->where(['login' => $login])->one();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return null;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
     }
 }
